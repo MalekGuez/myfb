23e31e75f42aaaf3991b7c8b5bfc11a5b3932cdb
Malek <rogex2542@gmail.com>
2021-12-14 17:25:00 +0100

    fix: client checkbox RGPD

--- myfbclient/vw-creer-compte.js
+++ myfbclient/vw-creer-compte.js
@@ -5,7 +5,7 @@ Vue.component( "vw-fil-actualite-ligne",{
 
 
 Vue.component( "vw-creer-compte",{
-    props:["connexion","db","nav"],
+    props:["connexion","db","nav", "checked"],
     template:`
         <div>
             <div>
@@ -24,6 +24,9 @@ Vue.component( "vw-creer-compte",{
                 <label>Mot de passe:</label>
                 <input type="text" size="10" v-model="connexion.mdp">
             </div>
+            <div class="checkbox">
+                <label><input type="checkbox" v-on:click="onCheckBoxClick()"><span>J'ai bien pris connaissance des recommandations RGPD concernant mes droits et devoirs</span></label>
+            </div>
                 <button v-on:click="onValider()">Ok</button>
                 <button v-on:click="onAnnuler()">Annuler</button>
             </div>
@@ -39,16 +42,33 @@ Vue.component( "vw-creer-compte",{
 
             // Construit le nouvel enregistrement
             this.connexion.id = id ;
-            // SQL insert
-            await this.db.insert( "users", this.connexion ) ;
 
-            // Revient vers vw-connexion
-            this.nav.currentViewId = 0 ;
+            if(this.connexion.compte === "" || this.connexion.nom === "" || this.connexion.prenom === "" || this.connexion.mdp === "") {
+                alert("Veuillez renseigner tous les champs");
+                this.nav.currentViewId = 2 ;
+            }
+            else if (!this.checked) {
+                alert("Veuillez confirmer avoir pris connaissance de vos droits et devoirs.");
+            }
+            else {
+                this.checked = true;
+                // SQL insert
+                await this.db.insert( "users", this.connexion ) ;
+                // Revient vers vw-connexion
+                this.nav.currentViewId = 0 ;
+            }
+
         },
         onAnnuler()
         {
             // Revient vers vw-connexion
             this.nav.currentViewId = 0 ;
+        },
+        onCheckBoxClick()
+        {
+            if(!this.checked)
+                this.checked = true;
+            else this.checked = false;
         }
     }
 }) ;
